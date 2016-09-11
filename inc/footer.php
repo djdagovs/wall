eb99ec642db58a3a796023e182f36bc4d658725a
Marcin <m.zog@icloud.com>
2016-09-11 15:39:18 +0100

    update

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,9 @@
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="js/bootstrap.js"></script>
 </body>
--- products.php
+++ products.php
@@ -57,9 +57,4 @@
 
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="js/bootstrap.js"></script>
-</body>
-</html>
+<?php include 'inc/footer.php'; ?>
